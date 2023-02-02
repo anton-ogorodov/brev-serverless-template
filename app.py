ad509a52ecf77ef561b4663cd01fbe8e60661cd2
antokas <antokas@gmail.com>
2023-02-02 09:28:48 +0000

    a

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
-from transformers import pipeline
+import os
 import torch
+import random
+import base64
+from io import BytesIO
+from transformers import pipeline
+from diffusers import StableDiffusionPipeline
+from diffusers.models import AutoencoderKL
+from diffusers import (
+    # DDPMScheduler,
+    # DDIMScheduler,
+    # PNDMScheduler,
+    # LMSDiscreteScheduler,
+    # EulerDiscreteScheduler,
+    # EulerAncestralDiscreteScheduler,
+    DPMSolverMultistepScheduler,
+)
+
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
-    global model
+    global model, vae
     
-    device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+    model_name = os.getenv("MODEL_NAME")
+    vae_name = os.getenv("VAE_NAME")
+    scheduler = DPMSolverMultistepScheduler.from_pretrained(model_name, subfolder="scheduler")
+    vae = AutoencoderKL.from_pretrained(vae_name, torch_dtype=torch.float16)
+    model = StableDiffusionPipeline.from_pretrained(model_name, vae=vae, scheduler=scheduler, torch_dtype=torch.float16).to("cuda")
+    
+    # model = StableDiffusionPipeline.from_pretrained(model_name).to("cuda")
+    
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
@@ -18,9 +40,26 @@ def inference(model_inputs:dict) -> dict:
     prompt = model_inputs.get('prompt', None)
     if prompt == None:
         return {'message': "No prompt provided"}
+
+    negative_prompt = model_inputs.get('negative_prompt', 'child, childish')
+    num_inference_steps = model_inputs.get('num_inference_steps', 30)
+    guidance_scale = model_inputs.get('guidance_scale', 7)
+    
+    width = model_inputs.get('width', 512)
+    height = model_inputs.get('height', 696)
+
+    random_seed = random.randint(0, 4294967295)
+    seed = model_inputs.get('seed', random_seed)
+    generator = torch.Generator("cuda").manual_seed(seed)
     
     # Run the model
-    result = model(prompt)
+    result = model(prompt, negative_prompt=negative_prompt, num_inference_steps=num_inference_steps, guidance_scale=guidance_scale, generator=generator, width=width, height=height)
+
+    # Check if result is an image or text
+    image = result.images[0]
+    buffered = BytesIO()
+    image.save(buffered,format="JPEG")
+    image_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
 
     # Return the results as a dictionary
-    return result
+    return {'image_base64': image_base64}
--- download.py
+++ download.py
@@ -1,13 +1,28 @@
 # In this file, we define download_model
 # It runs during container build time to get model weights built into the container
-
-# In this example: A Huggingface BERT model
-
-from transformers import pipeline
+import os, torch
+from diffusers import StableDiffusionPipeline
+from diffusers.models import AutoencoderKL
+from diffusers import (
+    # DDPMScheduler,
+    # DDIMScheduler,
+    # PNDMScheduler,
+    # LMSDiscreteScheduler,
+    # EulerDiscreteScheduler,
+    # EulerAncestralDiscreteScheduler,
+    DPMSolverMultistepScheduler,
+)
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    pipeline('fill-mask', model='bert-base-uncased')
+    model_name = os.getenv("MODEL_NAME")
+    vae_name = os.getenv("VAE_NAME")
+
+    scheduler = DPMSolverMultistepScheduler.from_pretrained(model_name, subfolder="scheduler")
+    vae = AutoencoderKL.from_pretrained(vae_name, torch_dtype=torch.float16)
+
+    model = StableDiffusionPipeline.from_pretrained(model_name, vae=vae, scheduler=scheduler, torch_dtype=torch.float16)
+    
 
 if __name__ == "__main__":
     download_model()
